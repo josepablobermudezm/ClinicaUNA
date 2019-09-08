f5dd1c986e03e4e9b0ffe13a707181381009b1ba
josepablobermudezm <53883209+josepablobermudezm@users.noreply.github.com>
2019-09-07 23:14:49 -0600

    a

--- src/clinicauna/controller/AgendaController.java
+++ src/clinicauna/controller/AgendaController.java
@@ -7,10 +7,15 @@ package clinicauna.controller;
 
 import com.jfoenix.controls.JFXDatePicker;
 import java.net.URL;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
@@ -18,6 +23,7 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 
 /**
  * FXML Controller class
@@ -44,12 +50,38 @@ public class AgendaController extends Controller{
     private Label labelyear;
     @FXML
     private Label labelmes;
-
+    @FXML
+    private Label lbl1;
+    @FXML
+    private Label labelSemana;
+    private String mes,year,semana;
+    @FXML
+    private Label lblDomingo;
+    @FXML
+    private Label lblLunes;
+    @FXML
+    private Label lblMartes;
+    @FXML
+    private Label lblMiercoles;
+    @FXML
+    private Label lblJueves;
+    @FXML
+    private Label lblViernes;
+    @FXML
+    private Label lblSabado;
   
 
     @Override
     public void initialize() {
 
+        Callback<DatePicker, DateCell> dayCellFactory = this.getDayCellFactory();
+        DatePicker.setDayCellFactory(dayCellFactory);
+        Inicio();
+            
+    }
+
+    public void Inicio(){
+        
         int valor = 1;
         for(int i = 0; i < 24; i++){
             VBox vPane = new VBox();
@@ -78,8 +110,45 @@ public class AgendaController extends Controller{
                 // Add it to the grid
                 calendarGrid.add(vPane, j, i);  
             }
-        }    
+        }
         
+    }
+    
+    @FXML
+    private void Fecha(Event event) {
+        mes = (DatePicker.getValue().getMonth()!=null)?DatePicker.getValue().getMonth().toString():" ";
+        year = (String.valueOf(DatePicker.getValue().getYear())!=null)?String.valueOf(DatePicker.getValue().getYear()):" ";
+        semana = (String.valueOf(DatePicker.getValue().getDayOfMonth())!=null)?String.valueOf(DatePicker.getValue().getDayOfMonth()):" ";
+        labelmes.setText(mes);
+        labelyear.setText(year);
+        labelSemana.setText(semana);
+        fechasDias();
+        
+    }
+    //Este metodo lo que hace es bloquer los días que no sean lunes del datepicker
+    private Callback<DatePicker, DateCell> getDayCellFactory() {
+        final Callback<DatePicker, DateCell> dayCellFactory = (final DatePicker datePicker) -> new DateCell() {
+            @Override
+            public void updateItem(LocalDate item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item.getDayOfWeek() != DayOfWeek.MONDAY ) {
+                    setDisable(true);
+                    setStyle("-fx-background-color: lightgray;");
+                }
+            }
+        };
+        return dayCellFactory;
+    }
+    
+    private void fechasDias(){
+        int diaMes = Integer.parseInt(semana);
+        lblDomingo.setText(lblDomingo.getText() + (diaMes-1));
+        lblLunes.setText(lblLunes.getText() + diaMes);
+        lblMartes.setText(lblMartes.getText() + (diaMes+1));
+        lblMiercoles.setText(lblMiercoles.getText() + (diaMes+2));
+        lblJueves.setText(lblJueves.getText() + (diaMes+3));
+        lblViernes.setText(lblViernes.getText() + (diaMes+4));
+        lblSabado.setText(lblSabado.getText() + (diaMes+5));
     }
     
 }
