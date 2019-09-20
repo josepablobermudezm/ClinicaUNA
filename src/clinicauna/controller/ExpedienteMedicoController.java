def029d2e7cca8a830c9d56c784967173c4aefd6
JordiRodriguezC <ycorrales120@gmail.com>
2019-09-19 22:44:57 -0600

    Las demás vistas

--- src/clinicauna/controller/ExpedienteMedicoController.java
+++ src/clinicauna/controller/ExpedienteMedicoController.java
@@ -5,18 +5,35 @@
  */
 package clinicauna.controller;
 
+import clinicauna.model.UsuarioDto;
+import clinicauna.util.AppContext;
+import clinicauna.util.Idioma;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 /**
  * FXML Controller class
  *
  * @author Jose Pablo Bermudez
  */
-public class ExpedienteMedicoController extends Controller{
+public class ExpedienteMedicoController extends Controller {
 
+    @FXML
+    private ImageView omg;
+    @FXML
+    private Label Titulo;
+    private Idioma idioma;
+    private UsuarioDto usuario;
 
     @Override
     public void initialize() {
-
+        idioma = (Idioma) AppContext.getInstance().get("idioma");
+        usuario = (UsuarioDto) AppContext.getInstance().get("UsuarioActivo");
+        if (usuario.getIdioma().equals("I")) {
+            this.Titulo.setText(idioma.getProperty("MedExp") + " " + idioma.getProperty("Expediente"));
+        }
 
     }
-    
+
 }
